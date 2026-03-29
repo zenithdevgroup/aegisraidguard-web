1dfa15a336365c63783b17de7be5469a17669524
ZenithDev <zenithdevgroup9@gmail.com>
2026-03-28 23:38:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const cards = document.querySelectorAll(".card");
+// CONTADOR FAKE PRO
+function animateValue(id, start, end, duration) {
+  let range = end - start;
+  let current = start;
+  let increment = end > start ? 1 : -1;
+  let stepTime = Math.abs(Math.floor(duration / range));
+  let obj = document.getElementById(id);
 
-window.addEventListener("scroll", () => {
-  cards.forEach(card => {
-    const rect = card.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 50) {
-      card.style.opacity = 1;
-      card.style.transform = "translateY(0)";
+  let timer = setInterval(() => {
+    current += increment;
+    obj.textContent = current.toLocaleString();
+    if (current == end) {
+      clearInterval(timer);
     }
-  });
-});
+  }, stepTime);
+}
+
+// valores simulados
+animateValue("servers", 0, 1287, 2000);
+animateValue("users", 0, 54213, 2000);
